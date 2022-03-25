0fa9c1988d62845c2fc216d50bf2f3769c405a8d
Vinicio Duca Bandeira <99352736+Vinicio-Duca@users.noreply.github.com>
2022-03-25 15:13:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function pegarDados(){
     let segFaixa = document.getElementById('2Faixa').value;
     let terFaixa = document.getElementById('3Faixa').value;
     let quarFaixa = document.getElementById('4Faixa').value;
-    let quinFaixa = document.getElementById('5Faixa').value;
     let con = parseFloat ( priFaixa + segFaixa + terFaixa );
     let resultado = con * quarFaixa;
     document.getElementById('resultado').innerHTML = resultado + ' Omhs';
